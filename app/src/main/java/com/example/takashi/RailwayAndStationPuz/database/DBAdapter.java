31e7ddd67da1156c47df77b05823cbab1b3ee467
PE-Nom <ib65629@gmail.com>
2017-04-14 00:03:01 +0900

    東日本75%の路線位置情報の正解データを登録

--- app/src/main/java/com/example/takashi/RailwayAndStationPuz/database/DBAdapter.java
+++ app/src/main/java/com/example/takashi/RailwayAndStationPuz/database/DBAdapter.java
@@ -22,7 +22,7 @@ import java.util.StringTokenizer;
 
 public class DBAdapter {
     static final String DATABASE_NAME = "Railway.db";
-    static final int DATABASE_VERSION = 67;
+    static final int DATABASE_VERSION = 69;
 
     private String TAG = "DBAdapter";
 
